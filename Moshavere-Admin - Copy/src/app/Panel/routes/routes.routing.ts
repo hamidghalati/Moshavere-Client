4b9d422c4a24c5452c711763058c9667126650dd
hamidghalati <umberela4u@yahoo.com>
2020-10-23 16:02:21 +0330

    1399.08.02

---
+++ Moshavere-Admin - Copy/src/app/Panel/routes/routes.routing.ts
@@ -0,0 +1,8 @@
+import { Routes, RouterModule } from '@angular/router';
+import { PanelComponent } from '../Panel.component';
+
+export const panelRoutes: Routes = [
+  {path: '', component: PanelComponent
+  },
+];
+
